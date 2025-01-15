fd129107cac1e8e2bc8acba8d34333722aea4717
xexeu.l <lugabriell084@outlook.com.br>
2025-01-15 17:20:47 -0300

    ADD: adicionei e alterei elementos no crud

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const { Curso } = require("./src/models/Curso");
 const alunos = new AlunoController()
 
 alunos.listarAluno();
+// alunos.adicionarAluno('Zeca pagodinho', 'pagodinho123@gmail.com', '89237483642', '8ss62j', 'tec');
